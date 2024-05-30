data-integrations/google-drive

column name cleansing done as per other file plugins.

src/main/java/io/cdap/plugin/google/sheets/source/GoogleSheetsSourceConfig.java
@@ -98,7 +98,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
   public static final String CONFIGURATION_PARSE_PROPERTY_NAME = "properties";
   private static final Logger LOG = LoggerFactory.getLogger(GoogleSheetsSourceConfig.class);
   private static final Pattern CELL_ADDRESS = Pattern.compile("^([A-Z]+)([0-9]+)$");
-  private static final Pattern COLUMN_NAME = Pattern.compile("^[A-Za-z_][A-Za-z0-9_-]*$");
+  private static final Pattern NOT_VALID_PATTERN = Pattern.compile("[^A-Za-z0-9_]+");
   private static LinkedHashMap<Integer, ColumnComplexSchemaInfo> dataSchemaInfo = new LinkedHashMap<>();
 
   @Name(SHEETS_TO_PULL)
@@ -593,7 +593,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
                                                                          int lastDataColumn,
                                                                          FailureCollector collector) {
     LinkedHashMap<Integer, ColumnComplexSchemaInfo> columnHeaders = new LinkedHashMap<>();
-
+    final Map<String, Integer> seenFieldNames = new HashMap<>();
     List<String> headerTitles = new ArrayList<>();
     for (int i = 0; i < Math.min(columnsRow.size(), lastDataColumn); i++) {
       CellData columnHeaderCell = columnsRow.get(i);
@@ -609,7 +609,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
       }
       String title = columnHeaderCell.getFormattedValue();
       if (StringUtils.isNotEmpty(title)) {
-        title = checkTitleFormat(title, i);
+        title = checkTitleFormat(title, seenFieldNames);
 
         // for merge we should analyse sub headers for data schemas
         if (isMergeHead) {
@@ -634,6 +634,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
   private List<ColumnComplexSchemaInfo> processSubHeaders(int startIndex, int length, List<CellData> subColumnsRow,
                                                           List<CellData> dataRow, FailureCollector collector) {
     List<ColumnComplexSchemaInfo> subHeaders = new ArrayList<>();
+    final Map<String, Integer> seenFieldNames = new HashMap<>();
     List<String> titles = new ArrayList<>();
     for (int i = startIndex; i < startIndex + length; i++) {
       String subHeaderTitle;
@@ -642,7 +643,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
         if (StringUtils.isEmpty(subHeaderTitle)) {
           subHeaderTitle = ColumnAddressConverter.getColumnName(i + 1);
         }
-        subHeaderTitle = checkTitleFormat(subHeaderTitle, i);
+        subHeaderTitle = checkTitleFormat(subHeaderTitle, seenFieldNames);
       } else {
         subHeaderTitle = ColumnAddressConverter.getColumnName(i + 1);
       }
@@ -661,14 +662,33 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
     return subHeaders;
   }
 
-  private String checkTitleFormat(String title, int columnIndex) {
-    if (!COLUMN_NAME.matcher(title).matches()) {
-      String defaultColumnName = ColumnAddressConverter.getColumnName(columnIndex + 1);
-      LOG.warn(String.format("Original column name '%s' doesn't satisfy column name requirements '%s', " +
-        "the default column name '%s' will be used.", title, COLUMN_NAME.pattern(), defaultColumnName));
-      return defaultColumnName;
+  private String checkTitleFormat(String title, Map<String, Integer> seenFieldNames) {
+    final String replacementChar = "_";
+
+    StringBuilder cleanFieldNameBuilder = new StringBuilder();
+
+    // Remove any spaces at the end of the strings
+    title = title.trim();
+
+    // If it's an empty string replace it with BLANK
+    if (title.isEmpty()) {
+      cleanFieldNameBuilder.append("BLANK");
+    } else if (Character.isDigit(title.charAt(0))) {
+      // Prepend a col_ if the first character is a number
+      cleanFieldNameBuilder.append("col_");
     }
-    return title;
+
+    // Replace all invalid characters with the replacement char
+    cleanFieldNameBuilder.append(NOT_VALID_PATTERN.matcher(title).replaceAll(replacementChar));
+
+    String cleanFieldName = cleanFieldNameBuilder.toString();
+    int count = seenFieldNames.getOrDefault(cleanFieldName, 0) + 1;
+    seenFieldNames.put(cleanFieldName, count);
+    // In case column already exists in seenFieldNames map, append the count with column name.
+    if (count > 1) {
+      cleanFieldNameBuilder.append(replacementChar).append(count);
+    }
+    return cleanFieldNameBuilder.toString();
   }
 
   private Schema getDataCellSchema(List<CellData> dataRow, int index, String headerName) {

festcloud/fc-google-drive

src/main/java/io/cdap/plugin/google/sheets/source/GoogleSheetsSourceConfig.java
@@ -98,7 +98,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
   public static final String CONFIGURATION_PARSE_PROPERTY_NAME = "properties";
   private static final Logger LOG = LoggerFactory.getLogger(GoogleSheetsSourceConfig.class);
   private static final Pattern CELL_ADDRESS = Pattern.compile("^([A-Z]+)([0-9]+)$");
-  private static final Pattern COLUMN_NAME = Pattern.compile("^[A-Za-z_][A-Za-z0-9_-]*$");
+  private static final Pattern NOT_VALID_PATTERN = Pattern.compile("[^A-Za-z0-9_]+");
   private static LinkedHashMap<Integer, ColumnComplexSchemaInfo> dataSchemaInfo = new LinkedHashMap<>();
 
   @Name(SHEETS_TO_PULL)
@@ -593,7 +593,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
                                                                          int lastDataColumn,
                                                                          FailureCollector collector) {
     LinkedHashMap<Integer, ColumnComplexSchemaInfo> columnHeaders = new LinkedHashMap<>();
-
+    final Map<String, Integer> seenFieldNames = new HashMap<>();
     List<String> headerTitles = new ArrayList<>();
     for (int i = 0; i < Math.min(columnsRow.size(), lastDataColumn); i++) {
       CellData columnHeaderCell = columnsRow.get(i);
@@ -609,7 +609,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
       }
       String title = columnHeaderCell.getFormattedValue();
       if (StringUtils.isNotEmpty(title)) {
-        title = checkTitleFormat(title, i);
+        title = checkTitleFormat(title, seenFieldNames);
 
         // for merge we should analyse sub headers for data schemas
         if (isMergeHead) {
@@ -634,6 +634,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
   private List<ColumnComplexSchemaInfo> processSubHeaders(int startIndex, int length, List<CellData> subColumnsRow,
                                                           List<CellData> dataRow, FailureCollector collector) {
     List<ColumnComplexSchemaInfo> subHeaders = new ArrayList<>();
+    final Map<String, Integer> seenFieldNames = new HashMap<>();
     List<String> titles = new ArrayList<>();
     for (int i = startIndex; i < startIndex + length; i++) {
       String subHeaderTitle;
@@ -642,7 +643,7 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
         if (StringUtils.isEmpty(subHeaderTitle)) {
           subHeaderTitle = ColumnAddressConverter.getColumnName(i + 1);
         }
-        subHeaderTitle = checkTitleFormat(subHeaderTitle, i);
+        subHeaderTitle = checkTitleFormat(subHeaderTitle, seenFieldNames);
       } else {
         subHeaderTitle = ColumnAddressConverter.getColumnName(i + 1);
       }
@@ -661,14 +662,34 @@ public class GoogleSheetsSourceConfig extends GoogleFilteringSourceConfig {
     return subHeaders;
   }
 
-  private String checkTitleFormat(String title, int columnIndex) {
-    if (!COLUMN_NAME.matcher(title).matches()) {
-      String defaultColumnName = ColumnAddressConverter.getColumnName(columnIndex + 1);
-      LOG.warn(String.format("Original column name '%s' doesn't satisfy column name requirements '%s', " +
-        "the default column name '%s' will be used.", title, COLUMN_NAME.pattern(), defaultColumnName));
-      return defaultColumnName;
+  private String checkTitleFormat(String title, Map<String, Integer> seenFieldNames) {
+    final String replacementChar = "_";
+
+    StringBuilder cleanFieldNameBuilder = new StringBuilder();
+
+    // Remove any spaces at the end of the strings
+    title = title.trim();
+
+    // If it's an empty string replace it with BLANK
+    if (title.isEmpty()) {
+      cleanFieldNameBuilder.append("BLANK");
+    } else if (Character.isDigit(title.charAt(0))) {
+      // Prepend a col_ if the first character is a number
+      cleanFieldNameBuilder.append("col_");
     }
-    return title;
+
+    // Replace all invalid characters with the replacement char
+    cleanFieldNameBuilder.append(NOT_VALID_PATTERN.matcher(title).replaceAll(replacementChar));
+
+    String cleanFieldName = cleanFieldNameBuilder.toString();
+    String lowerCaseCleanFieldName = cleanFieldName.toLowerCase();
+    int count = seenFieldNames.getOrDefault(lowerCaseCleanFieldName, 0) + 1;
+    seenFieldNames.put(lowerCaseCleanFieldName, count);
+    // In case column already exists in seenFieldNames map, append the count with column name.
+    if (count > 1) {
+      cleanFieldNameBuilder.append(replacementChar).append(count);
+    }
+    return cleanFieldNameBuilder.toString();
   }
 
   private Schema getDataCellSchema(List<CellData> dataRow, int index, String headerName) {